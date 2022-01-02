9da1c49bb7e9f1a0f9fe99193bd005c99f0d300e
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2022-01-02 15:12:19 -0300

    save favorite list on localStorage

--- src/store/modules/favoriteList/reducer.js
+++ src/store/modules/favoriteList/reducer.js
@@ -1,16 +1,18 @@
 import produce from "immer";
 
-const INITIAL_STATE = []
+const INITIAL_STATE =  JSON.parse(localStorage.getItem("@music:favoriteList")) ?? [];
 
 export const favoriteList = (state = INITIAL_STATE, action) => {
   return produce(state, draft => {
     switch (action.type) {
       case "ADD_MUSIC_TO_FAVORITE": {
         draft.push(action.payload);
+        localStorage.setItem("@music:favoriteList", JSON.stringify(draft));
         break;
       }
       case "REMOVE_MUSIC_FROM_FAVORITE": {
         draft.splice(draft.indexOf(action.payload), 1);
+        localStorage.setItem("@music:favoriteList", JSON.stringify(draft));
         break;
       }
       default: {
